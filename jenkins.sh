605dbe42455af58962dc2160e04fa82d33dc5a31
jiawei <jiawyao626@gmail.com>
2023-08-28 12:12:54 -0400

    Helm chart

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+docker build -t jwy626/demo .
+docker login
+docker push  jwy626/demo
+helm upgrade release-1 --install ./mychart --set apiversion=1.1
+helm upgrade release-2 --install ./mychart --set apiversion=1.2
+helm list
+sleep 5
+kubectl get svc | awk '{if ($4 != "<pending>" && NR>1)print $4}'
